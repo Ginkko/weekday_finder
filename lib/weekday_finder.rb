31a05fdf0b224d8c67705e00c79acc0b614a2429
Alec Arme & Jim Patt <student@epicodus.com>
2015-04-21 10:35:00 -0700

    Refactor and remove testing puts

--- lib/weekday_finder.rb
+++ lib/weekday_finder.rb
@@ -1,27 +1,19 @@
 class Time
   define_method(:weekday_finder) do
-    if
-      self.monday?()
+    if self.monday?()
       return "Monday"
-    elsif
-      self.tuesday?()
+    elsif self.tuesday?()
       return "Tuesday"
-    elsif
-      self.wednesday?()
+    elsif self.wednesday?()
       return "Wednesday"
-    elsif
-      self.thursday?()
+    elsif self.thursday?()
       return "Thursday"
-    elsif
-      self.friday?()
+    elsif self.friday?()
       return "Friday"
-    elsif
-      self.saturday?()
+    elsif self.saturday?()
       return "Saturday"
-    elsif
-      self.sunday?()
+    elsif self.sunday?()
       return "Sunday"
     end
   end
 end
-puts (Time.new(1987, 5, 21)).weekday_finder()
--- spec/weekday_finder_spec.rb
+++ spec/weekday_finder_spec.rb
@@ -7,6 +7,6 @@ require('pry')
 
 describe('Time#weekday_finder') do
   it("compares date to each day of the week") do
-    expect(Time.new(2015, 04, 21).weekday_finder).to(eq("Tuesday"))
+    expect(Time.new(2015, 04, 21).weekday_finder()).to(eq("Tuesday"))
   end
 end
