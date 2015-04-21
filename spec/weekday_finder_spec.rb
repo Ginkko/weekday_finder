c2feb3095146991b2d8f286effe100571968e368
Alec Arme & Jim Patt <student@epicodus.com>
2015-04-21 10:07:23 -0700

    Create specs

---
+++ spec/weekday_finder_spec.rb
@@ -0,0 +1,11 @@
+require('rspec')
+require('weekday_finder')
+require('pry')
+
+
+#Weekday finder comapres date to each day of the week.
+
+describe('Time#weekday_finder') do
+  it("compares date to each day of the week") do
+    expect(("2015, 04, 21").weekday_finder).to(eq("Tuesday"))
+  end
