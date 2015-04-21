30f117cd4decdde5b8e4682615156dc4d24aa61d
Alec Arme & Jim Patt <student@epicodus.com>
2015-04-21 10:30:08 -0700

    Code works for inputs outside of example spec

--- lib/weekday_finder.rb
+++ lib/weekday_finder.rb
@@ -6,6 +6,22 @@ class Time
     elsif
       self.tuesday?()
       return "Tuesday"
+    elsif
+      self.wednesday?()
+      return "Wednesday"
+    elsif
+      self.thursday?()
+      return "Thursday"
+    elsif
+      self.friday?()
+      return "Friday"
+    elsif
+      self.saturday?()
+      return "Saturday"
+    elsif
+      self.sunday?()
+      return "Sunday"
     end
   end
 end
+puts (Time.new(1987, 5, 21)).weekday_finder()
